47b30a32712113bbd103c87503da80666b92077c
FlamesW <undergodbreaker@gmail.com>
2025-11-29 09:41:10 +0200

    Update changelog entries for Grand Piece Online and Vellure

--- Resources/ChangelogsTab.lua
+++ Resources/ChangelogsTab.lua
@@ -9,13 +9,13 @@ return {
             ["Date"] = "29/11/2025",
             ["Changes"] = {
                 "- Added Grand Piece Online",
-                "- In BETA, Report any bugs",
+                "- Script In BETA, Report any bugs",
             },
             ["Note"] = "Note: Updates may take some time.",
         },
 
         {
-            ["Title"] = "Vellure [1.01]",
+            ["Title"] = "Vellure Library [1.01]",
             ["Date"] = "N/A",
             ["Changes"] = {
                 "- This is the beginning of our journey:)~",
