936f475edaa45d90c2d5c765429247811f1f1ad3
FlamesW <undergodbreaker@gmail.com>
2025-11-29 09:13:16 +0200

    Revise changelogs for Grand Piece Online and Vellure
    Updated game status and changelog entries for Vellure.

--- Resources/ChangelogsTab.lua
+++ Resources/ChangelogsTab.lua
@@ -1,11 +1,21 @@
 return {
     SupportedGames = {
-        {Game = "Grand Piece Online", Status = "Online", Date = "N/A"},
+        {Game = "Grand Piece Online", Status = "Online", Date = "29/11/2025"},
     },
 
     Changelogs = {
         {
-            ["Title"] = "Vellure 1.0",
+            ["Title"] = "Vellure 1.02",
+            ["Date"] = "29/11/2025",
+            ["Changes"] = {
+                "- Added Grand Piece Online",
+                "- In BETA, Report any bugs",
+            },
+            ["Note"] = "Note: Updates may take some time.",
+        },
+
+        {
+            ["Title"] = "Vellure 1.01",
             ["Date"] = "N/A",
             ["Changes"] = {
                 "- This is the beginning of our journey:)~",
